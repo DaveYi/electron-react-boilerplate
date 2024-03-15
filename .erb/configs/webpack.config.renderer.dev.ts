699652fe95d52535da36a282fcdfce9176bb645f
lpz <lipanzhong@qimao.com>
2024-03-15 17:59:12 +0800

    init

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -80,7 +80,17 @@ const configuration: webpack.Configuration = {
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: ['src/renderer/assets/style/global.scss'],
+            },
+          },
+        ],
         exclude: /\.module\.s?(c|a)ss$/,
       },
       // Fonts
